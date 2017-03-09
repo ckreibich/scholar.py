17797ddcaa24136d9363c658acad8b49744282e0
HesongHuang <huanghesong3231@yahoo.com>
2017-03-09 16:12:36 -0500

    Update scholar.py

--- scholar.py
+++ scholar.py
@@ -160,6 +160,7 @@ page. It is not a recursive crawler.
 # STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
 # IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.import optparse
+
 import optparse
 import os
 import re
